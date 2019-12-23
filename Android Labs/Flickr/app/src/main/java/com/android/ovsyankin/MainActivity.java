d3a8d6740cd9aeb401f3b1bf338e294d1d45cd8a
Raven124 <dikoti1994@yandex.ru>
2019-12-23 12:55:58 +0700

    Flickr
    Реализовано приложение с картинками использующее сервис Flickr. Также реализован поиск изображений

---
+++ Android Labs/Flickr/app/src/main/java/com/android/ovsyankin/MainActivity.java
@@ -0,0 +1,39 @@
+package com.android.ovsyankin;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.bittiger.janescookies.tinyflickr.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int KEEP_TIME = 2000;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "----------onCreate----------");
+
+        super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+        handler = new Handler();
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,  GalleryActivity.class);
+
+                Log.d(TAG, "----------startActivityIntent----------");
+                startActivity(intent);
+            }
+        }, KEEP_TIME);
+    }
+}
---
+++ Android Labs/Flickr/app/src/main/java/com/android/ovsyankin/SuggestionProvider.java
@@ -0,0 +1,17 @@
+package com.android.ovsyankin;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+public class SuggestionProvider extends SearchRecentSuggestionsProvider {
+
+    // part of content uri which is defined in AndroidManifest.xml
+    public static final String AUTHORITY = "com.android.bittiger.janescookies.tinyflickr" +
+            ".SuggestionProvider";
+
+    // suggestion mode which gives recent queries
+    public static final int MODE = DATABASE_MODE_QUERIES;
+
+    public SuggestionProvider() {
+        setupSuggestions(AUTHORITY, MODE);
+    }
+}
